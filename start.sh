f0cf231cb055cd795eced6994a540f13cc14a9b7
Gints Polis <gints.polis@lattelecom.lv>
2016-03-31 11:59:45 +0300

    postgres start also changed to 9.5

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
-su - postgres -c '/usr/pgsql-9.3/bin/postgres -D /var/lib/pgsql/data' & 
+su - postgres -c '/usr/pgsql-9.5/bin/postgres -D /var/lib/pgsql/data' & 
 if [[ ! -x /.db.init ]]; then
-	sleep 5
-	touch /.db.init
-	su - postgres -c 'createdb pdns'
-	su - postgres -c 'createuser pdns'
-	psql -U postgres -h localhost pdns < pdns.sql
+  sleep 5
+  touch /.db.init
+  su - postgres -c 'createdb pdns'
+  su - postgres -c 'createuser pdns'
+  psql -U postgres -h localhost pdns < pdns.sql
 fi
 sleep 4
 su - postgres -c '/node_modules/.bin/pgproc postgres://localhost:5432/pdns public 5380'  &
